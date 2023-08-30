42a7e284c9efd4b9f0aeccbc07727d2507911879
Vyko01 <Allucius.nkuna@gmail.com>
2023-08-30 03:50:36 -0700

    more

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0') /* base case */
 	{
-		_putchar('\n')
+		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
-	_puts_recursions(s + 1);
+	_puts_recursion(s + 1);
 }
