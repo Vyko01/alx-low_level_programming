a1bb7c12236b2d2267dfc029c17d78cb913b2c75
Vyko01 <Allucius.nkuna@gmail.com>
2023-09-05 00:12:22 -0700

    free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 
 /**
  * create_array - create array of size and assign char c
- * @size: size is array
+ * @size: size of array
  * @c: char to assign
  * Description: create array of size and assign char c
  * Return: pointer to array, NULL if fail
@@ -11,8 +11,8 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *str
-		unsigned int i;
+	char *str;
+	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
