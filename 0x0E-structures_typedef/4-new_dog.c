1db027ef258b67ddb6398e26700d20a1a0109ac2
Vyko01 <Allucius.nkuna@gmail.com>
2023-09-12 08:29:38 -0700

    prep

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	size_t n, o, i;
 	dog_t *dog;
 
-		if (name == NULL || owner == NULL)
+	if (name == NULL || owner == NULL)
 			return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
@@ -24,7 +24,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (n = 0; name[n]; n++)
 		;
 	n++;
-	dog->name = malloc(sizeof(char) * n);
+	dog->name = malloc(n * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
@@ -36,7 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (o = 0; owner[o]; o++)
 		;
 	o++;
-	dog->owner = malloc(sizeof(char) * o);
+	dog->owner = malloc(o * sizeof(char));
 	if (dog->owner == NULL)
 	{
 		free(dog->owner);
