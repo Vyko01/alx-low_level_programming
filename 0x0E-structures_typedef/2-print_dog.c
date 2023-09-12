eddfe4275442084e8907726a8913ee0ea00a682b
Vyko01 <Allucius.nkuna@gmail.com>
2023-09-12 07:36:10 -0700

    type

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (!((*d) .name))
-			printf("name; (nil)\n");
+		if (!((*d).name))
+			printf("Name; (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
