abe56ea2e49aae1fe9825962325f502bc054d022
Vyko01 <Allucius.nkuna@gmail.com>
2023-09-13 07:16:30 -0700

    prep

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,7 +8,7 @@
  * Return: a pointer to the function corresponding to the operator given
  */
 
-int (*get_op_function(char *s))(int, int)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
