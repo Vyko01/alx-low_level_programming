36e649f13df4ff9bb2a788f675cdd56027211f30
Vyko01 <allucius.nkuna@gmail.com>
2023-09-26 06:33:22 -0700

    singly

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  * Return: Pointer to the new node, or NULL.
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
 	unsigned int i;
 	listint_t *new;
