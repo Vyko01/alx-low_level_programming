7596fe3ccf7eb696871f3bf6273711e77495a151
Vyko01 <allucius.nkuna@gmail.com>
2023-10-05 05:47:36 -0700

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Otherwise - 1.
  */
 
 int create_file(const char *filename, char *text_content)
@@ -27,8 +27,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1 || w == -1)
 		return (-1);
-	
+
 	close(fd);
-	
+
 	return (1);
 }
